dd5e30ebc00fa79d713cf4140c5afd2cad77f2ae
Erne1984 <ernestofamorim@gmail.com>
2024-12-02 18:47:27 -0300

    refactor: deleteBookFromShelfs

--- src/controllers/ListController/deleteBookFromShelfs.js
+++ src/controllers/ListController/deleteBookFromShelfs.js
@@ -1,30 +1,27 @@
 const User = require("../../models/User");
 const List = require("../../models/List");
+const mongoose = require("mongoose");
 
 const deleteBookFromShelfs = async (req, res) => {
     try {
         const { userId, bookId } = req.body;
+
+        const bookObjectId = new mongoose.Types.ObjectId(bookId);
 
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).send({ error: "User not found" });
         }
 
-        user.toReadList = user.toReadList.filter(id => !id.equals(bookId));
-        user.readingList = user.readingList.filter(id => !id.equals(bookId));
-        user.readedList = user.readedList.filter(id => !id.equals(bookId));
-
+        user.toReadList = user.toReadList.filter(id => !id.equals(bookObjectId));
+        user.readingList = user.readingList.filter(id => !id.equals(bookObjectId));
+        user.readedList = user.readedList.filter(id => !id.equals(bookObjectId));
         await user.save();
 
         const lists = await List.find({ userId: user._id });
         for (const list of lists) {
-            list.booksIsbn = list.booksIsbn.filter(id => !id.equals(bookId));
-
-            if (list.booksIsbn.length === 0) {
-                await List.findByIdAndDelete(list._id);
-            } else {
-                await list.save();
-            }
+            list.books = list.books.filter(id => !id.equals(bookObjectId));
+            await list.save();
         }
 
         res.status(200).send({ message: "Book removed from all lists successfully" });
